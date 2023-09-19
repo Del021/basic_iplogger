eb0ef7e7c76b07055f7a1a3fc24aef7cf7aad4ef
Del0 <136930337+Del021@users.noreply.github.com>
2023-09-18 22:19:06 -0300

    Update color.py

--- python_files/color.py
+++ python_files/color.py
@@ -28,35 +28,6 @@ def intro():
     print(intro)
 
 
-def credits():
-    credits = f'''
-
-
-
-
-
-                                                         {green}   ⠀  ⠉⠙⠻⢿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-{purp}        ____     {cyan}    __                                    {green}   ⣀⣤⣶⣶⣦⣄⠙⣿⣿⣿⣇⣠⣶⣾⣿⣷⣶⣶⠄⠀⠀⠀⠀⠀
-{purp}       /  _/___  {cyan}   / /   ____  ___{yell}_ _____ ____  _____     {green} ⣠⣾⣿⣿⣿⣿⣿⣿⣷⣼⣿⣿⣿⣿⣿⣿⣿⠟⠋                      
-{purp}       / // __ \ {cyan}  / /   / __ \/ __{yell} `/ __ `/ _ \/ ___/     {green}⠘⠛⠉⠉⠉⠁⠉⠉⠛⢿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣤⣀⠀
-{purp}     _/ // /_/ / {cyan} / /___/ /_/ / /_/{yell} / /_/ /  __/ /           {green}   ⢀⣴⣿⠿⠛⢿⣿⣿⣿⣿⣟⠛⠻⢿⣷⣦⡀⠀⠀⠀
-{purp}    /___/ .___/  {cyan}/_____/\____/\__, {yell}/\__, /\___/_/           {green}   ⢠⣿⠀⠀   ⢸⣿⣿⡿⠻⣿⣷⡀⠀⠉⠻⢷
-{purp}       /_/       {cyan}           /____//{yell}____/                    {green}  ⢠⣿⡿⠁⠀⠀⠀⠸⣿⡿⠁⠀⠈⢿⣇⠀
-                                                         {green}    ⢠⣿⡿⠁     ⠉⠀⠀⠀⠀⠀⠏
-                                                             {green}⢠⣿⡿⠁
-                                                             {green}⢠⣿⣿⠁             {cyan}By: ~Del0
-                                                             {green}⢠⣿⣿⠇
-                                                            {green}⢀⣾⣿⡟
-                                                            {green}⣼⣿⣿⠃
-                                                           {green}⢠⣿⣿⡟
-
-
-
-
-                                                                                              
-'''
-    print(credits)
-
 
 
 
