dcce0ddaa8ad4d029f9ab293f80263f3340572c5
Del0 <136930337+Del021@users.noreply.github.com>
2024-01-05 14:09:55 -0300

    Create run.py

---
+++ run.py
@@ -0,0 +1,29 @@
+import main
+
+import color
+import importlib
+import os
+import logger
+import requests
+import sys
+from colorama import just_fix_windows_console, Fore, Style, Back
+
+
+#COLORES  
+red  = Fore.RED + Style.BRIGHT
+purp = Fore.MAGENTA  + Style.BRIGHT
+cyan = Fore.CYAN + Style.BRIGHT
+yell = Fore.YELLOW + Style.BRIGHT
+blue = Fore.BLUE + Style.BRIGHT
+back_yell = Fore.BLACK + Style.BRIGHT + Back.YELLOW
+green= Fore.GREEN + Style.BRIGHT
+#---------------------------------------------------
+
+
+
+def reset_mod(mod_name):
+    if mod_name in sys.modules:
+    	importlib.reload(sys.modules[mod_name])
+
+
+main.control()
